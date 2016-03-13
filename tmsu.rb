9fabc7a30a409f469de1edd05ab20ef2c8c2a093
Ian Levesque <ian@ianlevesque.org>
2016-03-13 11:48:09 -0400

    Homebrew formula for (head-only) tmsu.

---
+++ tmsu.rb
@@ -0,0 +1,23 @@
+require "language/go"
+
+class Tmsu < Formula
+  desc "TMSU lets you tags your files and then access them through a nifty virtual filesystem from any other application."
+  homepage "http://tmsu.org/"
+  head "https://github.com/oniony/TMSU.git"
+
+  depends_on "go" => :build
+  depends_on "godep" => :build
+  
+  go_resource "github.com/mattn/go-sqlite3" do
+    url "https://github.com/mattn/go-sqlite3.git"
+  end
+
+  def install
+    ENV["GOPATH"] = buildpath
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    system "go", "build", "-o", "bin/tmsu", "github.com/oniony/TMSU"
+    bin.install "bin/tmsu"
+    man1.install "misc/man/tmsu.1"
+  end
+end
